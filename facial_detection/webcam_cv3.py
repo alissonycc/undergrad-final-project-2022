b5f11f132f481aa6fe53ff244104acabd2e2832e
Alisson <alissoncarvalho@rocketmail.com>
2022-03-28 18:19:50 +0200

    gitignore + evaluate

---
+++ facial_detection/webcam_cv3.py
@@ -0,0 +1,63 @@
+import cv2
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
+import time
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+log.basicConfig(filename='webcam.log',level=log.INFO)
+
+teste = cv2.imread('/Users/Alisson/Documents/GitHub/undergrad-final-project-2022/facial_detection/imgs/brunna_e_jade.webp')
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+start = time.time()
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    #frameInstant = video_capture.grab()
+    resized = cv2.resize(teste, (48,48))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=7,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+        
+    # Display the resulting frame
+    frame[40:40+resized.shape[0], 40:40+resized.shape[1]] = resized
+    gray[40:40+resized.shape[0], 40:40+resized.shape[1]] = resized
+    cv2.imshow('Video', frame)
+    #cv2.imshow('Video', resized)
+    end = time.time()
+    if end-start >= 1:
+        cv2.imshow('Video', gray)
+        start = time.time()
+        
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
